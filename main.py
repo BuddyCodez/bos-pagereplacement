d714581da73029b4d06af3c05bbebc0699211a89
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-01-11 11:40:33 +0530

    new latest

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ import brain as Brain
 
 # ? eel.init('web') is used to initialize the GUI folder.
 eel.init('pagereplacement')
-@eel.expose
-def speak(text):
-    engine = pyttsx3.init()
-    voice = engine.getProperty('voices')
-    engine.setProperty('voice', voice[1].id)
-    engine.setProperty('rate', 170)
-    engine.say(text)
-    engine.runAndWait()
-    return text
 @eel.expose 
 def Algorithm(type, capacity,pageList):
     print(type,capacity,pageList)
