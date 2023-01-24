478fa04ec4d9eae1fbe1faf88ef37a0af0538bc9
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-01-24 20:08:01 +0530

    idk

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+import queue
 
+def mru(pages, frames):
+    q = queue.Queue()
+    page_faults = 0
+    recent = 0
+    for page in pages:
+        if page in q.queue:
+            recent = q.queue.index(page)
+        if page not in q.queue:
+            if q.qsize() == frames:
+                if recent == 0:
+                    q.queue.pop()
+                else:
+                    q.queue.remove(q.queue[recent])
+            q.put(page)
+            page_faults += 1
+    return page_faults
+
+li = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5]
+print(mru(li, 3))
